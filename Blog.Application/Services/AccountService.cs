e9a191473945466a2bfa6a0ff80a890db6368571
Manases Lovera <manaseslovera07@gmail.com>
2024-09-06 21:53:55 -0400

    AutoMapper and FluentValidation added

--- Blog.Application/Services/AccountService.cs
+++ Blog.Application/Services/AccountService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace api_dotnet.Blog.Infraestructure.Services
+namespace api_dotnet.Blog.Application.Services
 {
     public class AccountService
     {
