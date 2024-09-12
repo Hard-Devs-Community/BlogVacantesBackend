fc1f2830cd62c06b54178147d7af54d6bed191e7
Manases Lovera <manaseslovera07@gmail.com>
2024-09-12 12:02:48 -0400

    feat: databasecontext with base configuration

--- Blog.Infraestructure/Context/DatabaseContext.cs
+++ Blog.Infraestructure/Context/DatabaseContext.cs
@@ -2,12 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Infraestructure.Data
 {
     public class DatabaseContext : DbContext
     {
-        
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Vacant> Vacants { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+        }
     }
 }
